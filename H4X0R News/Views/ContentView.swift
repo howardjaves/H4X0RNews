37bb6ae620f21d040c55ab0976fc2fa25a722bf6
Howard Javes <howardjaves@gmail.com>
2021-07-18 10:35:17 +0100

    Finished project

---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Howard Javes on 17/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(
+                    destination: DetailView(url: post.url),
+                    label: {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
+                    })
+            }
+            .navigationBarTitle("H4X0R News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
